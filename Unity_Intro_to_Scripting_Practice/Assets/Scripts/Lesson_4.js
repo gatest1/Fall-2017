7df965c181c5f1e3734bdaf93e6a5def4c7628a3
Unknown <tayloranng@msn.com>
2017-08-24 08:59:05 -0600

    8/24/17-More Unity Practice-Operator types

---
+++ Unity_Intro_to_Scripting_Practice/Assets/Scripts/Lesson_4.js
@@ -0,0 +1,29 @@
+﻿/*Assignment operators:
+=
++=   x+=y (x=x+y)
+-=   x-+y (x=x-y)
+*=
+/=
+%=
+
+Arithmatic Operatiors:
++   addition
+-   subtraction
+*   multiplication
+/   division
+%   modulus  (division remainder)
+++  increment (+1)
+--  decrement (-1)
+
+*/
+
+
+var speed = 0;
+var distance = 200;
+var time = 3;
+
+function Update () 
+{
+	speed = distance / time;
+	Debug.Log ("You are going " + speed + "miles per hour.");
+}
---
+++ Unity_Intro_to_Scripting_Practice/Assets/Scripts/Lesson_5.js
@@ -0,0 +1,41 @@
+﻿/*Assignment operators:
+=
++=   x+=y (x=x+y)
+-=   x-+y (x=x-y)
+*=
+/=
+%=
+
+Arithmatic Operatiors:
++   addition
+-   subtraction
+*   multiplication
+/   division
+%   modulus  (division remainder)
+++  increment (+1)
+--  decrement (-1)
+
+Comparison Operators:
+==  is equal to...
+!=  not equal to...
+>   greater than...
+<   lesser than...
+>=  is greater than or equal to...
+<=  is less than or equal to...
+
+*/
+
+
+var speed = 0.0;
+var distance = 200.0;
+var time = 3.0;
+var speedLimit = 70.0;
+
+function Update () 
+{
+	speed = distance / time;
+	if(speed > speedLimit)
+	{
+	Debug.Log ("You are going over the speed limit.");
+	}
+}
