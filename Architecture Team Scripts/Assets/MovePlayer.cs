8e7fa0edaa35415baf851539e2fdd9377780834f
Unknown <tayloranng@msn.com>
2017-09-05 15:41:24 -0600

    9/5/2017- Rabbit dud, Rover, Star randomization

---
+++ Architecture Team Scripts/Assets/MovePlayer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	//This is the Character Controller Component
+	public CharacterController myCC;
+	//Temp var of datatype Vector3 to move the character
+	private Vector3 tempPos;
+	//Speed of the temp var in x
+	public float speed = 1;
+	public float gravity = 1;
+	public float jumpSpeed = 1;
+	public int jumpCount = 0;
+	public int jumpCountMax = 2;
+
+	// Update is called once per frame
+	void Update () {
+		//Waiting for input and comparing jumpcount
+		if (Input.GetKeyDown(KeyCode.Space) && jumpCount < jumpCountMax-1)
+		{
+			//incrementing the jumpcount by 1
+			jumpCount++;
+			//adding the jumpSpeed var to the tempPos var
+			tempPos.y = jumpSpeed;
+		}
+
+		//test if the Charactercontroller is gounded
+		if (myCC.isGrounded)
+		{
+			//reset the jump count if grounded
+			jumpCount = 0;
+		}
+		//adding the gravity bar to the y positionof the tempPos var
+		tempPos.y -= gravity;
+		//adding the speed var to the tempPos var x value with the right and left arrow keys
+		tempPos.x = speed * Input.GetAxis("Horizontal");
+        //adding the speed var to the tempPos var z value with the up and down arrow keys
+        tempPos.z = speed * Input.GetAxis("Vertical");
+		//Moves the Character Controller in at an even pace (deltaTime)
+		myCC.Move(tempPos * Time.deltaTime);  
+
+	}
+}
---
+++ Architecture Team Scripts/Assets/PickUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision col)
+    {
+         if(col.gameObject.name == "Pickup")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+}
