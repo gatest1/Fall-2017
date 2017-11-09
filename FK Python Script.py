7013d1c015b6b0f2be66c794d1be6e02d80c082f
Unknown <tayloranng@msn.com>
2017-11-09 13:59:10 -0700

    Rigging final...ish

---
+++ FK Python Script.py
@@ -0,0 +1,24 @@
+import maya.cmds as cmds
+
+cmds.SelectHierarchy()
+theJoints = cmds.ls(sl = True)
+theArraySize = len(theJoints)
+
+for i in range(len(theJoints)):
+    theCircleName = theJoints[i] + "_CTRL"
+    cmds.circle(n = theCircleName)
+    theCircle = cmds.ls (sl = True)
+    theGroupName = theJoints[i] + "_GRP"
+    cmds.group (n = theGroupName)
+    theGroup = cmds.ls(sl = True)
+    cmds.select(theJoints[i])
+    cmds.matchTransform(theGroupName, theJoints[i])
+    cmds.select(theGroup)
+    
+    if(i>0):
+        theParent = cmds.listRelatives(theJoints[i], p = True)
+        thePrevCircle = theParent[0] + "_CTRL"
+        cmds.select(thePrevCircle, tgl = True)
+        cmds.parent()
+    cmds.parentConstraint(theCircleName, theJoints[i])
+    cmds.scaleConstraint(theCircleName, theJoints[i], sc = False)
