82588c3b78d2a29e911c85829065db24a9de794e
Unknown <tayloranng@msn.com>
2017-12-14 12:14:07 -0700

    Scripting Final

--- MEL Maya Scripts/IK Python Script.py
+++ MEL Maya Scripts/IK Python Script.py
@@ -1,6 +1,6 @@
 # This is the Automated IK Python Script.
 # The IK handle needs to be created between the desired joints first
-# Then this script cann be run.
+# Then this script can be run.
 import maya.cmds as cmds
 
 ikHandlebar = cmds.ls (selection = True, type = 'ikHandle')
--- Toolbox_Official.py
+++ Toolbox_Official.py
@@ -1,4 +1,4 @@
-#This will be the UI Toolbox Script
+#This will be the Official UI Toolbox Script
 import maya.cmds as cmds
 import Broken_FK_Updated as brokenFK
 import FK_Updated as fkScript
@@ -8,25 +8,19 @@ import random
 def randomize(num, xRange, yRange, zRange): 
     sels = cmds.ls (sl= True)
     
-    for sel in sels:
+    for i in range(len(sels)):
         for x in range (0, (num + 1)):
-            xPos = random.randint ((xRange*-1), xRange)
-            yPos = random.randint ((yRange*-1), yRange)
-            zPos = random.randint ((zRange*-1), zRange)
+            xPos = random.randint (-xRange, xRange)
+            yPos = random.randint (-yRange, yRange)
+            zPos = random.randint (-zRange, zRange)
             
-            obj = cmds.duplicate (sel)
-            cmds.move (xPos, yPos, zPos)
-            cmds.rotate (xPos, yPos, zPos)
-    
-#Randomize (50, 50, 50, 50)
-
+            obj = cmds.duplicate (sels[i])
+            cmds.move (xPos, yPos, zPos, sels[i])
+            cmds.rotate (xPos, yPos, zPos, sels[i])
 
 #Sequential Renamer
-
 #Center Locator
-
 #Center Pivot
-
 
 #Delete History
 def deleteHistory():
@@ -34,13 +28,12 @@ def deleteHistory():
 #Freeze Transforms
 def freezeTransforms():
     cmds.FreezeTransformations()
-##Parent/Scale Constraints
+#Parent/Scale Constraints
 def parentscaleConstrain():
     cmds.parentConstraint ()
     cmds.scaleConstraint ()
 
 # Make a new window
-#
 def toolBoxWindow():
     window = cmds.window( title = "Tool Box", iconName = 'Short Name', widthHeight=(200, 55))
     cmds.rowColumnLayout()
@@ -50,14 +43,9 @@ def toolBoxWindow():
     #cmds.button( label='Center Pivot', c = cPivot())
     cmds.button( label='Delete History', c = 'deleteHistory()')
     cmds.button( label='Freeze Transforms', c = 'freezeTransforms()')
-    cmds.button( label='Parent/Scale Constraints', c = 'parentscaleContrain()')
+    cmds.button( label='Parent/Scale Constraints', c = 'parentscaleConstrain()')
     cmds.button( label='Auto FK Rig', c = 'fkScript.fkTool()')
     cmds.button( label='Broken FK', c = 'brokenFK.brokenFKTool()')
     cmds.showWindow ()
 
 toolBoxWindow()
-
-
-
-
-  
